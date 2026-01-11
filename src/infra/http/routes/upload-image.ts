a1b15208345bcf07f6ea1a6cfbe928c7904ea670
ketcode <kesse.matias@hotmail.com>
2026-01-11 15:51:45 -0300

    feat: add swagger and multipart support to fastify

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,7 +1,24 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import z from 'zod'
 
 export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
-  server.post('/uploads', async (request, reply) => {
-    return 'Hello World'
-  })
+  server.post(
+    '/uploads',
+    {
+      schema: {
+        summary: 'Upload an image',
+        body: z.object({
+          name: z.string(),
+          password: z.string().optional(),
+        }),
+        response: {
+          201: z.object({ uploadId: z.string() }),
+          409: z.object({ message: z.string() }).describe('Conflict error'),
+        },
+      },
+    },
+    async (request, reply) => {
+      return reply.status(201).send({ uploadId: 'teste' })
+    },
+  )
 }
