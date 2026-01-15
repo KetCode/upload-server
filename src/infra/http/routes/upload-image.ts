fb06407b52a274a949bb14495b4c8119a173e6f1
Kesse <64742407+KetCode@users.noreply.github.com>
2026-01-15 17:45:00 -0300

    feat: implement upload image functionality with validation and error handling

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,5 +1,4 @@
-import { db } from '@/infra/db'
-import { schema } from '@/infra/db/schemas'
+import { uploadImage } from '@/app/functions/upload-image'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import z from 'zod'
 
@@ -12,7 +11,7 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
         consumes: ['multipart/form-data'],
         response: {
           201: z.object({ uploadId: z.string() }),
-          409: z.object({ message: z.string() }).describe('Conflict error'),
+          400: z.object({ message: z.string() }),
         },
       },
     },
@@ -21,7 +20,15 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
         limits: { fileSize: 1024 * 1024 * 2 }, // 2MB
       })
 
-      console.log(uploadedFile)
+      if (!uploadedFile) {
+        return reply.status(400).send({ message: 'File is required' })
+      }
+
+      await uploadImage({
+        fileName: uploadedFile.filename,
+        contentType: uploadedFile.mimetype,
+        contentStream: uploadedFile.file,
+      })
 
       return reply.status(201).send({ uploadId: 'teste' })
     },
